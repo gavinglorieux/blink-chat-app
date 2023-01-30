a8cc846c57d780b39c5428440449d652c6991c81
Areeba Yusaf <areeba@MacBook-Pro.local>
2023-01-30 14:16:29 +0000

    Use ChatView and ChatList and set up state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState, useMemo } from "react";
+import chatData from "./data/chats.json";
+import { ChatAction, Message, Chat } from "./types";
+import ChatList from "./components/ChatList";
+import ChatView from "./components/ChatView";
+import "./App.css";
+
+const initialState: Chat[] = chatData;
+
+const reducer = (state: Chat[], action: ChatAction) => {
+  const { type, payload, chatId } = action;
+
+  switch (type) {
+    case "add-message":
+      return state.map((chat) => {
+        if (chatId === chat.id) {
+          return {
+            ...chat,
+            messages: [...chat.messages, { ...payload }],
+            last_updated: payload.last_updated,
+          };
+        } else {
+          return chat;
+        }
+      });
+    case "edit-message":
+      return state.map((chat) => {
+        if (chatId === chat.id) {
+          const messages = chat.messages.map((msg) => {
+            return msg.id === payload.id ? payload : msg;
+          });
+          return { ...chat, messages, last_updated: payload.last_updated };
+        } else {
+          return chat;
+        }
+      });
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [chatState, dispatch] = useReducer(reducer, initialState);
+  const [selectedChatId, setSelectedChatId] = useState<string | null>(null);
+  const [selectedMessage, setSelectedMessage] = useState<Message | null>(null);
+
+  const selectedChat = useMemo(
+    () => chatState.find((chat: Chat) => chat.id === selectedChatId),
+    [chatState, selectedChatId]
+  );
+
+  const onSendMessage = (chatId: string, message: Message) => {
+    dispatch({ type: "add-message", payload: message, chatId });
+  };
+
+  const onEditMessage = (chatId: string, message: Message) => {
+    dispatch({ type: "edit-message", payload: message, chatId });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="chat-app">
+      <div className="chat-app__chat-list">
+        <ChatList
+          chats={chatState}
+          onClickChat={(id: string) => {
+            setSelectedChatId(id);
+            setSelectedMessage(null);
+          }}
+          selectedChatId={selectedChatId}
+        />
+      </div>
+      <div className="chat-app__message-view">
+        {selectedChat && (
+          <ChatView
+            chat={selectedChat}
+            selectedMessage={selectedMessage}
+            onSetMessage={setSelectedMessage}
+            onSendMessage={onSendMessage}
+            onEditMessage={onEditMessage}
+          />
+        )}
+      </div>
     </div>
   );
 }
